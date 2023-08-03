50ab4d1d3cb6929c105310f7eb21c1aaa4c2a35b
Michelle M <michellemalfabon@gmail.com>
2023-08-02 19:03:40 -0700

    navtoggle

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,12 +1,15 @@
 import { HStack, Image, Text } from '@chakra-ui/react'
 import React from 'react'
 import logo from '../assets/logo.webp'
+import ColorModeSwitch from './ColorModeSwitch'
 
 const NavBar = () => {
   return (
     <HStack>
       <Image src={logo} boxSize='60px'/>
-      <Text>Nav Bar</Text>
+
+      <ColorModeSwitch/>
+      
     </HStack>
   )
 }
