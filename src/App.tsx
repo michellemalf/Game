1adcef9089e17b3b87ca9c835c45d08b35291037
Michelle Malfabon <89042600+michellemalf@users.noreply.github.com>
2023-08-02 18:31:29 -0700

    initial

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+import { Button, ButtonGroup, Grid, GridItem, Show } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <Grid
+      templateAreas={{
+        base: `'nav' 'main'`,
+        lg: `'nav nav' 'aside main'`, // 1024px
+      }}
+    >
+      <GridItem area="nav" bg="orange">
+        Nav
+      </GridItem>
+      <Show above='lg'>
+      <GridItem area="aside" bg="gold">
+        Aside
+      </GridItem>
+      </Show>
+      <GridItem area="main" bg="blue">
+        Main
+      </GridItem>
+    </Grid>
+  );
+}
+
+export default App;
