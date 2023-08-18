175fa5b0a489ed079da787570baaa4c6ad51f762
Michelle M <michellemalfabon@gmail.com>
2023-08-17 19:55:21 -0700

    platform added

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -5,10 +5,11 @@ import { Button, HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui
 import getCroppedImageUrl from "../services/image-url";
 
 interface Props{
-  onSelectGenre: (genre: Genre) => void
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null
 }
 
-const GenreList = ({onSelectGenre}: Props) => {
+const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
 
   const {data, isLoading, error} = useGenres();
   
@@ -21,7 +22,7 @@ const GenreList = ({onSelectGenre}: Props) => {
         <HStack>
             <Image boxSize='32px' borderRadius={8} src={getCroppedImageUrl(genre.image_background)}></Image>
             {/* <Text fontSize='lg'>{genre.name}</Text> */}
-            <Button onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
+            <Button color={genre.id === selectedGenre?.id ? 'blue.500': 'normal'} onClick={()=> onSelectGenre(genre)} fontSize='lg' variant='link'>{genre.name}</Button>
         </HStack>
         </ListItem>
       ))}
